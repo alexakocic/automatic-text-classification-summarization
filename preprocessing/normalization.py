df2efeaab6be2b9250d6c5f40f4919cd14fe18c5
alexakocic <aleksa.kocic993@gmail.com>
2017-11-18 20:11:53 +0100

    Added tokenize_sentences method and blank tokenize_words method in normalization.py module

--- preprocessing/normalization.py
+++ preprocessing/normalization.py
@@ -1,9 +1,26 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Nov 18 17:18:51 2017
 
 @author: Aleksa Kocić
 
-Text normalization module. Contains methods for transforming raw text 
-into normalized version of it.
+Text normalization module. Contains methods for transforming raw text into 
+normalized version of it.
 """
+
+import nltk
+
+def tokenize_sentences(text):
+    """
+    For a given text, retrieve list of sentences it is made of.
+    
+    Args:
+        text (str): Input text
+    
+    Returns:
+        list: List of text sentences that input text is made of
+    """
+    return nltk.tokenize.sent_tokenize(text)
+
+# TODO: not implemented
+def tokenize_words(sentence):
+    return False 
