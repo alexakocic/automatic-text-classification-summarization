47526b1311ad742d439e97fe0b0d240d57d1d54c
alexakocic <aleksa.kocic993@gmail.com>
2017-11-20 20:28:59 +0100

    Fixed not commiting everything in previous commit

--- preprocessing/normalization.py
+++ preprocessing/normalization.py
@@ -8,6 +8,7 @@ normalized version of it.
 """
 
 import nltk
+import spell_check as sc
 
 def tokenize_sentences(text):
     """
@@ -17,10 +18,32 @@ def tokenize_sentences(text):
         text (str): Input text
     
     Returns:
-        list: List of text sentences that input text is made of
+        list: List of sentences that input text is made of
     """
     return nltk.tokenize.sent_tokenize(text)
 
-# TODO: not implemented
 def tokenize_words(sentence):
-    return False 
+    """
+    For a given sentence, retrieve list of words it is made of.
+    
+    Args:
+        sentence (str): Input sentence
+    
+    Returns:
+        list: List of words that input sentence is made of
+    """
+    return nltk.tokenize.word_tokenize(sentence)
+
+def tokenize(text):
+    """
+    For a given text, retrieve list of all words it consists of.
+    
+    Args:
+        text (str): Input text
+    
+    Returns:
+        list: List of words that input text is made of
+    """
+    return [word for sentence in tokenize_sentences(text) 
+            for word in tokenize_words(sentence)]
+    
